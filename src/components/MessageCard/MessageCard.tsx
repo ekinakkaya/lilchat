5040ae01d780cbcd34032064c2a1b1da5530fd36
Ekin Akkaya <ekoeko0654@gmail.com>
2024-11-19 01:43:25 +0300

    fixed syntaxhighlighter overloader error

--- src/components/MessageCard/MessageCard.tsx
+++ src/components/MessageCard/MessageCard.tsx
@@ -18,11 +18,10 @@ export const MessageCard = ({ role, message }: ChatMessage) => {
           children={message}
           components={{
             code(props) {
-              const { children, className, node, ...rest } = props;
+              const { children, className, ...rest } = props;
               const match = /language-(\w+)/.exec(className || "");
               return match ? (
                 <SyntaxHighlighter
-                  {...rest}
                   PreTag="div"
                   children={String(children).replace(/\n$/, "")}
                   language={match[1]}
